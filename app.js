e38117f413b083e3781c989b5df27b87d050e347
oyilmaztekin <ozer.yilmaztekin@n11.com>
2019-12-22 12:32:37 +0300

    added statics and ejs

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const http = require("http");
 const indexRouter = require("./routes/index");
 
 app.use("/", indexRouter);
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 const server = http.createServer(app);
 server.listen(3000, "127.0.0.1", () => {
-  console.log("listening the port 127.0.0.1:3000")
-}
-);
+  console.log("listening the port 127.0.0.1:3000");
+});
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,14 @@ router.get("/", async (req, res, next) => {
   try {
     const response = await fetch("http://localhost:5000/api/userList");
     const data = await response.json();
-    res.send(data);
+    
+    res.render("index", {
+      userList: data,
+      title: "Kullanıcı Bilgileri"
+    });
+    
   } catch (error) {
-      res.send('birşeyler ters gitti.');
+    res.send("birşeyler ters gitti.");
   }
 });
 
