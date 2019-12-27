bc008a28b21007761d64eced52e9ebb8e37a4fff
Ozer.Yilmaztekin <ozer.yilmaztekin@n11.com>
2019-12-27 20:35:39 +0300

    completed starter

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', ({res}) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on("connection", (socket) => {
+    console.log('------------------------------------------------------');
+    console.log(`${socket.id} - Kullanıcı bağlandı`);
+    socket.on('disconnect', () => console.log(`${socket.id} - Kullanıcı çıkış yaptı.`));
+})
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
+  
